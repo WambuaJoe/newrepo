e23cdb03b30e824611d3e182e58b92657b766f46
Modasoka <kaytujohn254@gmail.com>
2022-10-17 12:08:15 +0300

    switch ops

---
+++ months.c
@@ -0,0 +1,28 @@
+// this program is used to determine the current month using the switch statement
+
+#include <stdio.h>
+
+int main() {
+    int Month = 4;
+
+    switch (Month) {
+        case 1:
+            printf("January\n");
+            break;
+        case 2:
+            printf("February\n");
+            break;
+        case 3:
+        printf("March\n");
+            break;
+        case 4:
+        printf("April\n");
+            break;
+        case 5:
+        printf("May\n");
+            break;
+        case 6:
+        printf("June\n");
+            break;
+    }
+}
