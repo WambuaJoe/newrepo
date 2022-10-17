3f8d7e35bb90bbe14a5ea3cc35526a23076da7da
Modasoka <kaytujohn254@gmail.com>
2022-10-17 20:30:28 +0300

    .\new_switch.c

---
+++ new_switch.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char card_name[3];
+    puts("Enter card name: ");
+    fgets(card_name, sizeof(card_name), stdin);
+    puts("Card name: ");
+    puts(card_name);
+    int val = 0;
+
+    switch (card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+        val = 10;
+        break;
+    case 'A':
+    val = 11;
+        val = 11;
+        break;    
+    default:
+        val = atoi(card_name);
+        break
+        ;
+    }
+}
