dc8c6e5aca2fd2bc0e8748c3bc624bf12c06c4b0
Modasoka <kaytujohn254@gmail.com>
2022-10-19 12:22:52 +0300

    new

--- codecamp.c
+++ codecamp.c
@@ -1,9 +1,23 @@
-#include <stdlib.h>
-#include <stdio.h>
-// program to get input from a user
-int main() {
+#include "main.h"
+
     
-   int Number[10];
+   struct codecamp {
+    char StudName[20];
+    char course[20];
+    char StudID[20];
+    double score;
+   };
    
+int main() {
+    struct codecamp student1;
+    strcpy(student1.StudID, "a101-ALX");
+    strcpy(student1.course, "Introduction to C programming");
+    strcpy(student1.StudName, "Joseph Wambua");
+    student1.score = 134.43;
+
+    printf("%f", student1.score);
+    printf("%c", student1.StudID);
+    printf("%c", student1.course);
+    printf("%c", student1.StudName);
     return 0;
 }
