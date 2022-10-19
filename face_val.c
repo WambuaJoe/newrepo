9aeb7777ebb76bf586c511ca333a13cfcb14614c
Modasoka <kaytujohn254@gmail.com>
2022-10-19 23:20:58 +0300

    fixed issues with linking structure elements to execute the program

--- face_val.c
+++ face_val.c
@@ -1,31 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+
+
 
 int main() {
-    char card_name[3];
-    puts("Enter card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    if (card_name[0] == 'K') {
-        val = 10;
-    }
-    else if (card_name[0] == 'Q') {
-        val = 10;
-    }
-    else if (card_name[0] == 'J') {
-        val = 10;
-    }
-    else if (card_name[0] == 'A') {
-        val = 11;
-    }
-    else {
-        val = atoi(card_name);
-    }
+  int a = 10;
+  int* p;
+  p = &a;
 
-    if ((val > 2) && (val < 7))
-        puts("Count has gone up");
-    else if(val == 10)
-        puts("Count has gone down");
-    
-        return 0;
+  printf("%d\n", p);
+  printf("size of integer is %d bytes\n", sizeof(int));
+  printf("%d\n", (p + 1));
 }
