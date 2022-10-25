ea795313cbb8708c2ff6276ef4506a53bdafaf44
Modasoka <kaytujohn254@gmail.com>
2022-10-25 15:17:35 +0300

    further insight into pointers and pointer arithmetic

--- pointer.c
+++ pointer.c
@@ -2,12 +2,9 @@
 
 int main() {
     int a = 10;
-    int *ptr = &a;
-
-    printf("address of p: %d\n", ptr);
-    printf("value at address of p: %d\n", *ptr);
-    printf(" size of integer is %d\n", sizeof(int));
-    printf("address of p+1 is %d\n", ptr + 1);
-    printf("value at address of p+1 is %d\n", *(ptr + 1));
-
+    int *ptr;
+    ptr = &a;
+    printf("address of p: %d\n", a);
+    *ptr = 5;
+    printf("value of ptr: %d\n", a);
 }
