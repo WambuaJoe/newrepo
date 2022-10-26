06a20f297ca3b2b49197931f9e273a5cb5a4e15c
Modasoka <kaytujohn254@gmail.com>
2022-10-26 11:27:40 +0300

    new

--- pointer.c
+++ pointer.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
 int main() {
-    int a = 10;
+   /* int a = 10;
     int *ptr;
     ptr = &a;
-    printf("address of p: %d\n", a);
-    *ptr = 5;
-    printf("value of ptr: %d\n", a);
+    printf("address: %d\n", ptr);
+    printf("value at address ptr: %d\n", *ptr);
+    printf("integer size: %d\n",sizeof(int));
+    printf("address ptr+1: %d\n", ptr+1);
+    printf("value at address ptr+1: %d\n", *(ptr+1)); */
+
+    int a = 1025;
+    int *ptr;
+    ptr = &a;
+    printf("address: %d\n", ptr);
+    printf("value at address ptr: %d\n", *ptr);
+    printf("integer size: %d\n",sizeof(int));
+
 }
