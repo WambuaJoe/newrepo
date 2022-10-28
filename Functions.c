d026884d4795b65fde4b8264d6fcfb3d2a01246c
Modasoka <kaytujohn254@gmail.com>
2022-10-28 07:08:20 +0300

    better understanding of functions anad recursion

---
+++ Functions.c
@@ -0,0 +1,47 @@
+// defining, declaring and calling functions
+#include "header.h"
+/*
+type functName(parameter1, parameter2, parameterN) {
+    //block of code
+    return(); //only if function is defined without a void type
+}
+
+void mabonzo(char bonzo[]) {
+    printf("Mbwa wewe %s\n", bonzo);
+}
+
+int main() {
+    mabonzo("Wambua");
+    return 0;
+}
+*/
+// function that returns a result after parameter arithmetic after function call
+int funct1(int j, int k) {
+    return 5 * (j * k);
+}
+
+int main() {
+    int total = funct1(10, 20);
+    printf("total is : %d\n", total);
+    
+    return 0;
+}
+
+// function declaration and definition
+/*
+type functName(parameter1, parameter2, paremeterN); //declaration
+
+//main method
+int main() {
+    returnType variable = functName(parameter1, parameter2, parameterN);    //invoke function
+    // block of code
+    return 0;
+}
+
+//definition
+
+type functName(parameter1, parameter2, parameterN) {
+    return (parameter operationName);
+}
+
+*/
---
+++ header.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Prototypes
+
+
+//Structures
+
+
+//Functions
+
+
+#endif
