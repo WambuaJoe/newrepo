ed92e9c4d5d16e0b7ff64f4a6d3c26665d6d7d20
Modasoka <kaytujohn254@gmail.com>
2022-11-16 20:21:58 +0300

    pointer operations

--- temp.c
+++ temp.c
@@ -1,16 +1,26 @@
 #include "header.h"
 
-int main() {
-	char chr;
-	printf("Enter character: ");
-	scanf("%c", &chr);
+void pMax(int a, int b);
 
-	if (chr == 'J') {
-		printf("You did it!\n");
-		exit(0);
+int main() {
+	void pMax();
+	
+	for (int i = -10; i <= 10; i++) {
+		for (int j = -10; j <= 10; j++) {
+			pMax(i, j);
+		}
+	}
+	exit(EXIT_SUCCESS);
+}
+
+void pMax(int a, int b) {
+	int mobiD;
+
+	if (a > b) {
+		mobiD = a;
 	}
 	else {
-		printf("Wrong character, you fucking twat\n");
+		mobiD = b;
 	}
-	return 0;
+	printf("larger of %d and %d is %d\n", a, b, mobiD);
 }
