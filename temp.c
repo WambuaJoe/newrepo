14b45d84ed9482cc0ad89b2b3588ab3851e45148
WambuaJoe <kaytujohn254@gmail.com>
2022-12-06 18:14:45 +0300

    linked lists ops

--- temp.c
+++ temp.c
@@ -1,5 +1,5 @@
 #include "header.h"
-
+/*
 void pMax(int a, int b);
 
 int main() {
@@ -23,4 +23,44 @@ void pMax(int a, int b) {
 		mobiD = b;
 	}
 	printf("larger of %d and %d is %d\n", a, b, mobiD);
+}
+*/
+
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+int is_palindrome(listint_t **head)
+{
+        listint_t *tmp = *head;
+        int values, i = 0, cLoop, limit;
+
+        if (head == NULL || *head == NULL)
+                return (1);
+        while (tmp != NULL)
+        {
+                i++;
+                tmp = tmp->next;
+        }
+        values = malloc(sizeof(int) * i);
+        if (values == NULL)
+                return (0);
+
+        tmp = *head;
+        for (cLoop = 0; cLoop < i; cLoop++)
+        {
+                values[cLoop] = tmp->n;
+                tmp = tmp->next;
+        }
+
+        limit = i /2;
+        for (cLoop = 0; cLoop < limit; cLoop++)
+                if (values[cLoop] != values[i - 1 - cLoop])
+                {
+                        free(values);
+                        return (0);
+                }
+        free(values);
+        return (1);
 }
